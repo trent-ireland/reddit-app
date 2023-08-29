accc5f5a3c9a1017fc98939cb83e86a264323638
trent-ireland <trentireland@87gmail.com>
2023-08-29 11:13:05 +0100

    updated css

--- src/Components/Header/SearchResults.js
+++ src/Components/Header/SearchResults.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import './SearchBar.js'
-import 'SearchResults.css'
+import React, { useState } from 'react';
+import SubredditPosts from './SubredditPosts';
+import './SearchResults.css';
+
 function SearchResults({ subreddits }) {
+  const [selectedSubreddit, setSelectedSubreddit] = useState(null);
+
+  const handleSubredditClick = (subredditName) => {
+    setSelectedSubreddit(subredditName);
+  };
+
   return (
-    <ul>
-      {subreddits.map((subreddit) => (
-        <li key={subreddit.name}>
-          {subreddit.name} - Subscribers: {subreddit.subscribers}
-        </li>
-      ))}
-    </ul>
+    <div className="search-results">
+      <ul>
+        {subreddits.map((subreddit) => (
+          <li
+            key={subreddit.name}
+            onClick={() => handleSubredditClick(subreddit.name)}
+          >
+            {subreddit.name} - Subscribers: {subreddit.subscribers}
+          </li>
+        ))}
+      </ul>
+      {selectedSubreddit && <SubredditPosts subredditName={selectedSubreddit} />}
+    </div>
   );
 }
 
